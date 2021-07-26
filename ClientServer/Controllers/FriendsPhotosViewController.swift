d142dda02ec1588be4cc7df4ab72864cfef7fcd3
Kirya333 <kirill24062000@mail.ru>
2021-07-26 19:02:26 +0300

    1. Создать учетную запись в Firebase.
    2. Интегрировать в приложение.
    
    3. Настроить изменение данных в Firebase без авторизации.
    
    4. Записывать в базу пользователей, которые авторизовались в приложении (id).
    
    5. Записывать каждому пользователю группы, которые он добавлял в приложении.

--- ClientServer/Controllers/FriendsPhotosViewController.swift
+++ ClientServer/Controllers/FriendsPhotosViewController.swift
@@ -6,38 +6,58 @@
 //
 
 import UIKit
+import RealmSwift
 
 class FriendsPhotosViewController: UIViewController {
     
     @IBOutlet weak var friendPhotosCollectionView: UICollectionView!
     
-    
-    var photos = [Photo]()
-    var friendID: Int = 0
-    
     let apiService = VKService()
+    let realmService = RealmService()
+    
+    var token: NotificationToken?
     
     let friendPhotosCollectionViewCellIdentifier = "FriendPhotosCollectionViewCellIdentifier"
     let fromFriendsPhotosToGallerySegueIdentifier = "fromPhotosToGallery"
     
+    var photos: Results<PhotoModel>? {
+        didSet {
+            token = photos?.observe { [weak self] changes in
+                guard let self = self else { return }
+                
+                switch changes {
+                case .initial:
+                    self.friendPhotosCollectionView.reloadData()
+                case .update(_, let deletions, let insertions, let modifications):
+                    self.friendPhotosCollectionView.performBatchUpdates({
+                        self.friendPhotosCollectionView.insertItems(at: insertions.map({ IndexPath(row: $0, section: 0) }))
+                        self.friendPhotosCollectionView.deleteItems(at: deletions.map({ IndexPath(row: $0, section: 0) }))
+                        self.friendPhotosCollectionView.reloadItems(at: modifications.map({ IndexPath(row: $0, section: 0) }))
+                    }, completion: nil)
+                case .error(let error):
+                    fatalError("\(error)")
+                }
+            }
+        }
+    }
+    
+    var friendID: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setPhotoBy(userID: friendID)
+        setPhotosBy(userId: friendID)
         
         friendPhotosCollectionView.dataSource = self
         friendPhotosCollectionView.delegate = self
         friendPhotosCollectionView.register(UINib(nibName: "FriendPhotosCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: friendPhotosCollectionViewCellIdentifier)
     }
     
-    func setPhotoBy(userID: Int) {
-        apiService.getPhotos(by: userID) { [weak self] photos in
-            guard let self = self else { return }
-            self.photos = photos
-            self.friendPhotosCollectionView.reloadData()
-            
-        }
+    func setPhotosBy(userId: Int) {
+        apiService.getPhotos(by: userId, completion: <#([Photo]) -> ()#>)
+        
+        guard let realm = try? Realm() else { return }
+        photos = realm.objects(PhotoModel.self).filter("ownerID == \(userId)")
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -55,29 +75,24 @@ class FriendsPhotosViewController: UIViewController {
 
 
 extension FriendsPhotosViewController: UICollectionViewDataSource, UICollectionViewDelegate {
-    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return photos.count
+        return photos?.count ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: friendPhotosCollectionViewCellIdentifier, for: indexPath) as? FriendsPhotosCollectionViewCell else {
-            
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: friendPhotosCollectionViewCellIdentifier, for: indexPath) as? FriendsPhotosCollectionViewCell
+        else {
             return UICollectionViewCell()
-            
+        }
+    
+        if let photo = photos?[indexPath.row] {
+            cell.configure(photo: photo)
         }
         
-        let photo = photos[indexPath.row]
-        
-        cell.configure(photo: photo)
-        
         return cell
-        
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         performSegue(withIdentifier: fromFriendsPhotosToGallerySegueIdentifier, sender: indexPath)
     }
-    
 }
