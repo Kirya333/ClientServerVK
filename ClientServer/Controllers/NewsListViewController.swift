13de34ff4516383321170d75c9b409f101952c7c
Kirya333 <kirill24062000@mail.ru>
2021-08-20 17:39:10 +0300

    1. Реализовать и применить все три паттерна в вашей ленте новостей. 2. Ограничить максимальный размер текста 200 точками. Если текста меньше — отобразить его целиком. 3. Добавить функциональность отображения текста целиком. Для этого добавить в ячейку текста кнопку Show more. Нажатие на нее будет изменять высоту ячейки с текстом так, чтобы текст отображался полностью. 4. Превратить кнопку Show more в Show less, нажатие на которую будет возвращать ячейке с текстом исходный вид.

--- ClientServer/Controllers/NewsListViewController.swift
+++ ClientServer/Controllers/NewsListViewController.swift
@@ -21,20 +21,54 @@ class NewsListViewController: UIViewController {
     private var news = [FirebaseNews]()
     private let ref = Database.database().reference(withPath: "news/\(Session.shared.userId)")
     
+    var nextFrom = ""
+    var isLoading = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setNews()
+        setupRefreshControl()
+        
         
         newsTableView.dataSource = self
+        newsTableView.prefetchDataSource = self
+        newsTableView.delegate = self
+        newsTableView.allowsSelection = false
+        
         newsTableView.register(UINib(nibName: "NewsTableViewImageCell", bundle: nil), forCellReuseIdentifier: newsTableViewCellImageIdentifier)
         newsTableView.register(UINib(nibName: "NewsTableViewTextCell", bundle: nil), forCellReuseIdentifier: newsTableViewCellTextIdentifier)
         newsTableView.register(UINib(nibName: "NewsTableViewCountersCell", bundle: nil), forCellReuseIdentifier: newsTableViewCellCountersIdentifier)
         
     }
     
+    fileprivate func setupRefreshControl() {
+        newsTableView.refreshControl = UIRefreshControl()
+        newsTableView.refreshControl?.attributedTitle = NSAttributedString(string: "Refreshing...")
+        newsTableView?.tintColor = #colorLiteral(red: 0.1773500971, green: 1, blue: 0, alpha: 1)
+        newsTableView.refreshControl?.addTarget(self, action: #selector(refreshNews), for: .valueChanged)
+    }
+    
+    @objc func refreshNews() {
+        newsTableView.refreshControl?.beginRefreshing()
+        
+        let mostFreshNewsDate = self.news.first?.date ?? Int(Date().timeIntervalSince1970)
+        
+        apiService.getNewsfeed(startTime: mostFreshNewsDate) { [weak self] nextFrom in
+            guard let self = self else { return }
+            self.nextFrom = nextFrom
+        }
+        
+        newsTableView.refreshControl?.endRefreshing()
+        
+    }
+    
     func setNews() {
-        apiService.getNewsfeed()
+        apiService.getNewsfeed() { [weak self] nextFrom in
+            guard let self = self else { return }
+            self.nextFrom = nextFrom
+            
+        }
         
         ref.observe(.value, with: { [weak self] snapshot in
             guard let self = self else { return }
@@ -47,6 +81,10 @@ class NewsListViewController: UIViewController {
                 }
             }
             
+            news.sort(by: { $0.date > $1.date })
+            
+            news.forEach({ $0.calculateTextHeight(from: self.view.frame.width)})
+            
             self.news = news
             self.newsTableView.reloadData()
         })
@@ -54,7 +92,7 @@ class NewsListViewController: UIViewController {
     
 }
 
-extension NewsListViewController: UITableViewDataSource {
+extension NewsListViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return news.count
     }
@@ -64,6 +102,27 @@ extension NewsListViewController: UITableViewDataSource {
         if news[section].text != nil { rows += 1 }
         if news[section].urlImage != nil { rows += 1}
         return rows
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let new = news[indexPath.section]
+        
+        if new.text != nil && indexPath.row == 0 {
+            if new.heightText < NewsTableViewTextCell.defaultTextHeight {
+                return new.heightText + NewsTableViewTextCell.smallIndent
+            } else if new.isExpanded {
+                return new.heightText + NewsTableViewTextCell.buttonHeight + NewsTableViewTextCell.smallIndent * 2
+            } else {
+                return NewsTableViewTextCell.defaultTextHeight + NewsTableViewTextCell.buttonHeight + NewsTableViewTextCell.smallIndent * 2
+            }
+        } else if (new.text == nil && new.urlImage != nil && indexPath.row == 0) ||
+                    (new.text != nil && new.urlImage != nil && indexPath.row == 1) {
+            let tableWidth = tableView.bounds.width
+            let cellHeight = tableWidth * new.apectRatio + NewsTableViewImageCell.smallIndent
+            return cellHeight
+        } else {
+            return UITableView.automaticDimension
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -76,6 +135,7 @@ extension NewsListViewController: UITableViewDataSource {
                 return UITableViewCell()
             }
             cell.configure(news: new)
+            cell.delegate = self
             
             return cell
             
@@ -96,5 +156,34 @@ extension NewsListViewController: UITableViewDataSource {
             
             return cell
         }
+    }
+}
+
+
+extension NewsListViewController: UITableViewDataSourcePrefetching {
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        guard let maxSection = indexPaths.map({ $0.section }).max() else { return }
+        
+        if maxSection > news.count - 3, !isLoading {
+            isLoading = true
+            
+            apiService.getNewsfeed(startFrom: nextFrom) { [weak self] nextFrom in
+                guard let self = self else { return }
+                self.nextFrom = nextFrom
+                
+                self.isLoading = false
+                
+            }
+        }
+    }
+}
+
+extension NewsListViewController: NewsTableViewTextCellDelegate {
+    func showMoreAction(cell: NewsTableViewTextCell) {
+        guard let indexPath = newsTableView.indexPath(for: cell) else { return }
+        let new = news[indexPath.section]
+        
+        new.isExpanded = !new.isExpanded
+        newsTableView.reloadRows(at: [indexPath], with: .automatic)
     }
 }
